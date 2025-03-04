57061113a005b9c65b3ceaaaaf81b31af30cc587
Stephanie Herr <stephanie.herr@red-gate.com>
2025-03-03 23:59:27 -0500

    first commit

---
+++ Product.sql
@@ -0,0 +1,5 @@
+CREATE TABLE [dbo].[Product]
+(
+    [ProductID] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY,
+    [ProductName] [nvarchar] NOT NULL
+);-- Write your own SQL object definition here, and it'll be included in your package.
